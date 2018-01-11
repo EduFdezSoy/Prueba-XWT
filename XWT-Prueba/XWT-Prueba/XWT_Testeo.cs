79c8b60c7744ed63810eac216f3bcd36c45af231
Adrian Muñoz Mudarra <eladrixhd@gmail.com>
2018-01-11 18:00:46 +0100

    Comezando con los testeos

---
+++ XWT-Prueba/XWT-Prueba/XWT_Testeo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xwt;
+
+
+namespace XWT_Prueba
+{
+    class XWT_Testeo
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Initialize(ToolkitType.Gtk);
+            var mainWindow = new Window()
+            {
+                Title = "Xwt Demo Application",
+                Width = 877,
+                Height = 418
+            };
+            mainWindow.Show();
+            Application.Run();
+            mainWindow.Dispose();
+        }
+    }
+}
